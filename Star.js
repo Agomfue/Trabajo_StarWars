a7f5a75bb02efeb4c70c437043f9236a6ba42d82
Jesusgit-hub <jes2579goz@gmail.com>
2026-04-14 09:13:43 +0200

    Update Star.js

--- Star.js
+++ Star.js
@@ -1,16 +1,1 @@
-  /* - NAVES - */
-  // Crear naves dinámicas
-  const usuarios = [
-   { id: 1, nombre: "Halcón Milenario", rol: "Halcón Milenario" },
-   { id: 2, nombre: "Ala-X", rol: "Ala-X" },
-   { id: 3, nombre: "Ala-A", rol: "Ala-A" },
-   { id: 4, nombre: "Ala-B", rol: "Ala-B" },
-   { id: 5, nombre: "Ala-Y", rol: "Ala-Y" },
-   { id: 6, nombre: "El Espíritu", rol: "El Espíritu" },
-   { id: 7, nombre: "Transporte Rebelde", rol: "Transporte Rebelde" },
-   { id: 8, nombre: "Transporte GR-75", rol: "Transporte GR-75" },
-   { id: 9, nombre: "Crucero Mon Calamari", rol: "Crucero Mon Calamari" },
-   { id: 10, nombre: "Fragata Nebulon-B", rol: "Fragata Nebulon-B" },
-   { id: 11, nombre: "Corbeta Corelliana", rol: "Corbeta Corelliana" }
-    
-];
+
