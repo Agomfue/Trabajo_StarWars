020b52b5327d103db828470ecd677f46df7c14da
Agomfue <abelgomez98206@gmail.com>
2026-04-14 11:52:51 +0200

    Ocultar secciones

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
 /*Se hara un inner html donde se modificar las opciones y luego seleccionarlas con un switch*/
 
+/*Para hacer que las secciones se oculten y aparezacan al hacer click*/
+const hangarEnl = document.getElementById("hangarEnl");
+const pilotosEnl = document.getElementById("pilotosEnl");
+const misionesEnl = document.getElementById("misionesEnl");
+const dashboardEnl = document.getElementById("dashboardEnl");
+const hangar = document.getElementById("hangar");
+const pilotos = document.getElementById("pilotos");
+const misiones = document.getElementById("misiones");
+const dashboard = document.getElementById("dashboard");
+
+/*Lo que hace es añadir la clase oculto a los elementos*/
+function ocultarTodo() {
+    hangar.classList.add("oculto");
+    pilotos.classList.add("oculto");
+    misiones.classList.add("oculto");
+    dashboard.classList.add("oculto");
+}
+
+hangarEnl.addEventListener("click", function() {
+    ocultarTodo();/*oculta todas las secciones*/
+    hangar.classList.remove("oculto");/*Le quita la clase oculto a la seccion*/
+});
+
+pilotosEnl.addEventListener("click", function() {
+    ocultarTodo();
+    pilotos.classList.remove("oculto");
+});
+
+misionesEnl.addEventListener("click", function() {
+    ocultarTodo();
+    misiones.classList.remove("oculto");
+});
+
+dashboardEnl.addEventListener("click", function() {
+    ocultarTodo();
+    dashboard.classList.remove("oculto");
+});
+
+
+/*Para crear el formulario*/
 let nombre = document.getElementById("nombrePil");
 let rango = document.getElementById("rangoPil");
 let nave = document.getElementById("select");
