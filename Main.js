aeae0d496eb0ddbd47925ab9c1845bcf298eac2f
Jesusgit-hub <jes2579goz@gmail.com>
2026-04-20 12:34:13 +0200

    Subida de main

---
+++ Main.js
@@ -0,0 +1,91 @@
+// - NAVES DE LA ALIANZA REBELDE - 
+const naves = [
+  { 
+    nombre: "Halcón Milenario", 
+    tipo: "transporte", 
+    velocidad: 80, 
+    tripulacion: 4, 
+    estado: "operativa", 
+    imagen: "El-Halcón-Milenario-Star-Wars.jpg"  
+  },
+  { 
+    nombre: "Ala-X (X-Wing)", 
+    tipo: "caza", 
+    velocidad: 100, 
+    tripulacion: 1, 
+    estado: "operativa", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Ala-A (A-Wing)", 
+    tipo: "caza", 
+    velocidad: 120, 
+    tripulacion: 1, 
+    estado: "en reparación", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Ala-B (B-Wing)", 
+    tipo: "caza", 
+    velocidad: 90, 
+    tripulacion: 1, 
+    estado: "operativa", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Ala-Y (Y-Wing)", 
+    tipo: "bombardero", 
+    velocidad: 80, 
+    tripulacion: 2, 
+    estado: "en reparación", 
+    imagen: "" 
+  },
+  { 
+    nombre: "El Espíritu (Ghost)", 
+    tipo: "transporte", 
+    velocidad: 85, 
+    tripulacion: 6, 
+    estado: "operativa", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Transporte Rebelde", 
+    tipo: "transporte", 
+    velocidad: 60, 
+    tripulacion: 5, 
+    estado: "operativa", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Transporte GR-75", 
+    tipo: "transporte", 
+    velocidad: 50, 
+    tripulacion: 6, 
+    estado: "destruida", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Crucero Mon Calamari", 
+    tipo: "crucero", 
+    velocidad: 60, 
+    tripulacion: 5400, 
+    estado: "operativa", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Fragata Nebulon-B", 
+    tipo: "fragata", 
+    velocidad: 40, 
+    tripulacion: 850, 
+    estado: "operativa", 
+    imagen: "" 
+  },
+  { 
+    nombre: "Corbeta Corelliana", 
+    tipo: "corbeta", 
+    velocidad: 70, 
+    tripulacion: 165, 
+    estado: "operativa", 
+    imagen: "" 
+  }
+];
